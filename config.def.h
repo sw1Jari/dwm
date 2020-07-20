e99730aaee76f7576535b20df754130a1423463e
Jari Kasandiredjo <jarikasan@gmail.com>
2020-07-20 15:17:47 +0200

    sane binds

--- config.def.h
+++ config.def.h
@@ -61,7 +61,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = +1 } },
